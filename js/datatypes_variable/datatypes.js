1041a7f206864ab889d491975b99db8a338984d3
Gurpal gora <gs6750722@gmail.com>
2026-06-12 11:55:14 +0530

    inital commit

---
+++ js/datatypes_variable/datatypes.js
@@ -0,0 +1,27 @@
+// data types in Js:
+
+// primitive data types : number, string,boolean,float,null,undefined
+// non -primitivte data types : array,object
+
+// let  a =10 number 
+// let b = "name" string 
+//  let c =  true boolean 
+//  let d= 4.5 float /double 
+//  let x= 4.5;
+//   console.log( typeof x);
+
+// primitve  data 
+
+let arr=[44,56,36,36,21]
+let arr2 = arr;
+ arr[0] = 10
+console.log(arr2);
+
+// non-primitive data
+// let name="Mohan"
+// name[0]="R" alphabad Will not be a change 
+// name="Rohan" updated 
+// console.log(name);
+
+
+  
