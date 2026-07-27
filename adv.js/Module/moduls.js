b2297233a7582f61ac6ac26463d4cac27af41260
Gurpal gora <gs6750722@gmail.com>
2026-07-27 12:43:58 +0530

    inital commit

---
+++ adv.js/Module/moduls.js
@@ -0,0 +1,11 @@
+export default function greet(){
+    console.log("hello")
+}
+function add(a,b){
+    console.log(a+b)
+}
+function sub(a,b){
+    console.log(a-b)
+}
+let name="Gurpal"
+export{add,sub,name}  
